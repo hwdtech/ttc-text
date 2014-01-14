e173cc3c4fc077feb94038f693639ceb35aaafee
Maxim Malov <mmalov@hwdtech.ru>
2014-01-14 16:10:51 +0700

    License year fix

--- test/TextString.spec.js
+++ test/TextString.spec.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2011-2013 Limited Liability Company "Tik-Tok Coach"
+ * Copyright (C) 2014 Limited Liability Company "Tik-Tok Coach"
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
